b308c79786f9162e13e629423a3889882253b9eb
Anand kumar pandey <pandeyanand200123@gmail.com>
2026-04-01 07:45:05 +0530

    fix: express 5 wildcard for catch-all route

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("(.*)", (req, res) => {
     res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
   });
 }
